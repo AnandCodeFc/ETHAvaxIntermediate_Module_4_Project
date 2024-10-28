bc62d4a34a62be35f9fa3c96814a98bb998c2e67
AnandCodeFc <anandcoder54@gmail.com>
2024-10-28 13:16:58 -0400

    Update Module4-GamingContract.sol

--- Module4-GamingContract.sol
+++ Module4-GamingContract.sol
@@ -1,1 +1,62 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.10;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract GameCloneToken is ERC20 {
+    address public gameOwner;
+
+    mapping(uint256 => string) public itemDescriptions;
+    mapping(uint256 => uint256) public itemCosts;
+    mapping(address => mapping(uint256 => bool)) public purchasedItems;
+    mapping(address => uint256) public totalPurchasedItems;
+
+    constructor() ERC20("Degen", "DGN") {
+        gameOwner = msg.sender;
+        _initializeStore(0, "sticker", 500);
+        _initializeStore(1, "phone", 1000);
+        _initializeStore(2, "laptop", 20000);
+        _initializeStore(3, "servers", 25000);
+    }
+
+    modifier onlyOwner() {
+        require(gameOwner == msg.sender, "Only Have Access To Execute this function!");
+        _;
+    }
+
+    event Purchase(address indexed user, string item);
+
+    function _initializeStore(uint256 itemId, string memory _itemDescription, uint256 _itemCost) internal {
+        itemDescriptions[itemId] = _itemDescription;
+        itemCosts[itemId] = _itemCost;
+    }
+
+    function addItem(uint256 itemId, string memory _itemDescription, uint256 _itemCost) public onlyOwner {
+        _initializeStore(itemId, _itemDescription, _itemCost);
+    }
+
+    function mintTokens(uint256 amount) public onlyOwner {
+        _mint(msg.sender, amount);
+    }
+
+    function burnTokens(uint256 amount) public {
+        _burn(msg.sender, amount);
+    }
+
+    function purchaseItem(uint256 itemId) public returns (string memory) {
+        uint256 cost = itemCosts[itemId];
+        require(balanceOf(msg.sender) >= cost, "Not enough tokens to purchase the item.");
+
+        _burn(msg.sender, cost);
+        purchasedItems[msg.sender][itemId] = true;
+        totalPurchasedItems[msg.sender]++;
+        emit Purchase(msg.sender, itemDescriptions[itemId]);
+
+        return itemDescriptions[itemId];
+    }
+
+    function getTotalPurchasedItems(address user) public view returns (uint256) {
+        return totalPurchasedItems[user];
+    }
+}
 
